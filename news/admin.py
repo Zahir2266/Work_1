1c7de1c7e16057f4f4b989d349aae5820eabd9c0
Zahir2266 <vpapaiuny@gmail.com>
2024-06-03 10:16:20 +0700

    Admin panel (+user +group +news)

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from news.models import News
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'content',)
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from users.models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", 'group', ]
+    search_fields = ["first_name", "last_name", "email", 'group', ]
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
