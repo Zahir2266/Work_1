6540b42b7a754b6cb1d64e5cb1e53381df4dd2fe
Zahir2266 <vpapaiuny@gmail.com>
2024-06-03 10:06:05 +0700

    News model (+news)

--- news/models.py
+++ news/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import *
+
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    STATUS_CHOICES = (
+        (1, 'Draft'),
+        (2, 'Moderation'),
+        (3, 'ReadyPublication'),
+        (3, 'Publish'),
+    )
+    status = models.IntegerField(
+        choices=STATUS_CHOICES,
+        verbose_name='Статус')
+
+    author = models.ForeignKey(to=User, verbose_name='Статус')  # limit_choices_to={'group : 1'},
+    moderator = models.ForeignKey(to=User, verbose_name='Модер')  # limit_choices_to={'group : 1'},
